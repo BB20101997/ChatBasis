1e98999c524d306b090626f8c8b1c81b71a97ac0
BB20101997 <blessmann-bennet@t-online.de>
2014-11-24 20:55:58 +0100

    LoginPacket used to login the user SignUpPacket "obviously" to signup SavePacket to save force the Server to Save

---
+++ src/bb/chat/network/packet/Command/SavePacket.java
@@ -0,0 +1,7 @@
+package bb.chat.network.packet.Command;
+
+/**
+ * Created by BB20101997 on 24.11.2014.
+ */
+public class SavePacket {
+}
---
+++ src/bb/chat/network/packet/Handshake/LoginPacket.java
@@ -0,0 +1,7 @@
+package bb.chat.network.packet.Handshake;
+
+/**
+ * Created by BB20101997 on 23.11.2014.
+ */
+public class LoginPacket {
+}
