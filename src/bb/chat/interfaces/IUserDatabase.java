f7f4b76e6e79150f0abb4622c1caa269fdd204a3
BB20101997 <blessmann-bennet@t-online.de>
2014-11-30 00:36:09 +0100

    added som functions to the interfaces

--- src/bb/chat/interfaces/IUserDatabase.java
+++ src/bb/chat/interfaces/IUserDatabase.java
@@ -1,7 +1,18 @@
 package bb.chat.interfaces;
 
+import bb.util.file.database.ISaveAble;
+
 /**
  * Created by BB20101997 on 26.11.2014.
  */
-public class IUserDatabase {
+public interface IUserDatabase<U extends IUser> extends ISaveAble {
+
+	U getUserByID(int i);
+
+	U getUserByName(String s);
+
+	void addUserToDatabase(U u);
+
+	void removeUserFromDatabase(U u);
+
 }
