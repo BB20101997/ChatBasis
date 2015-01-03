b40becbf0f68d612198754844f934fb39232ea64
BB20101997 <blessmann-bennet@t-online.de>
2015-01-03 01:23:04 +0100

    changed according to ICommand and added Subcommand System for this Command

--- src/bb/chat/command/Permission.java
+++ src/bb/chat/command/Permission.java
@@ -1,31 +1,49 @@
 package bb.chat.command;
 
-import bb.chat.enums.Side;
+import bb.chat.command.Subcommands.Permission.*;
+import bb.chat.command.Subcommands.Permission.Group.*;
+import bb.chat.command.Subcommands.Permission.Help;
+import bb.chat.command.Subcommands.Permission.User.*;
 import bb.chat.interfaces.ICommand;
 import bb.chat.interfaces.IMessageHandler;
-import bb.chat.interfaces.IPacket;
-import bb.chat.network.packet.PermissionPacket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by BB20101997 on 28.11.2014.
  */
 public class Permission implements ICommand {
 
-	private static final String[] alias = new String[]{"permission-deny-add", "permission-allow-add", "permission-deny-remove", "permission-allow-remove", "permission-group-add", "permission-group-remove", "permission-help"};
+	public List<SubPermission> subCommandList = new ArrayList<>();
 
-	@Override
-	public int maxParameterCount() {
-		return 2;
-	}
-
-	@Override
-	public int minParameterCount() {
-		return 2;
+	public Permission(){
+		subCommandList.add(new Help(this));
+		subCommandList.add(new Create());
+		subCommandList.add(new Delete());
+		subCommandList.add(new UserPermAdd());
+		subCommandList.add(new UserPermRemove());
+		subCommandList.add(new UserDenyAdd());
+		subCommandList.add(new UserDenyRemove());
+		subCommandList.add(new UserGroupAdd());
+		subCommandList.add(new UserGroupRemove());
+		subCommandList.add(new GroupCreate());
+		subCommandList.add(new GroupDelete());
+		subCommandList.add(new GroupPermAdd());
+		subCommandList.add(new GroupPermRemove());
+		subCommandList.add(new GroupDenyAdd());
+		subCommandList.add(new GroupDenyRemove());
+		subCommandList.add(new GroupAddGroup());
+		subCommandList.add(new GroupRemoveGroup());
 	}
 
 	@Override
 	public String[] getAlias() {
-		return alias;
+		String[] s = new String[subCommandList.size()];
+		for(int i = 0; i < s.length; i++) {
+			s[i] = subCommandList.get(i).getName();
+		}
+		return s;
 	}
 
 	@Override
@@ -37,14 +55,12 @@ public class Permission implements ICommand {
 	public void runCommand(String commandLine, IMessageHandler imh) {
 		String[] command = commandLine.split(" ", 3);
 
-		if("permission-help".equals(command[0])) {
-			//TODO:Add help
-		} else {
-			if(imh.getSide() == Side.CLIENT) {
-				IPacket p = new PermissionPacket(command[0], command[1], command[2]);
-				imh.sendPackage(p);
-			} else {
-				imh.getPermissionRegistry().setPermission(IMessageHandler.SERVER, imh.getUserByName(command[1]), command[0], command[2]);
+		if(!"permission".equals(command[0])) {
+			for(SubPermission sC : subCommandList) {
+				if(("permission-" + sC.getName()).equals(command[0])) {
+					sC.runCommand(commandLine, imh);
+					break;
+				}
 			}
 		}
 	}
