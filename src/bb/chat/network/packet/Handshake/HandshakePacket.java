4d80a2e843ec634a52c792147e53fba455bea7bb
BB20101997 <blessmann-bennet@t-online.de>
2015-01-27 21:04:59 +0100

    added a differention between a Client/User connection and other connections e.g. another Server

--- src/bb/chat/network/packet/Handshake/HandshakePacket.java
+++ src/bb/chat/network/packet/Handshake/HandshakePacket.java
@@ -13,18 +13,26 @@ import java.io.IOException;
 public class HandshakePacket extends IPacket {
 
 	private String Version = "1.0";
+	private boolean userClient;
 
 	public HandshakePacket(){
 		minNetworkState = NetworkState.UNKNOWN;
 	}
 
+	public HandshakePacket(boolean user){
+		minNetworkState = NetworkState.UNKNOWN;
+		userClient = user;
+	}
+
 	@Override
 	public void writeToData(DataOut dataOut) throws IOException {
 		dataOut.writeUTF(Version);
+		dataOut.writeBoolean(userClient);
 	}
 
 	@Override
 	public void readFromData(DataIn dataIn) throws IOException {
 		Version = dataIn.readUTF();
+		userClient = dataIn.readBoolean();
 	}
 }
