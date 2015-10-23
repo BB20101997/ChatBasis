3f2db7516b083da233b4fe6cfc0f0be8cc0bcbbd
BB20101997 <blessmann-bennet@t-online.de>
2015-10-24 00:11:34 +0200

    comments added changing to logFolder is now validated against stupidity

--- src/bb/chat/chat/InitLoggers.java
+++ src/bb/chat/chat/InitLoggers.java
@@ -4,6 +4,9 @@ import bb.util.file.LogHandler;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.NotDirectoryException;
+import java.rmi.AccessException;
+import java.rmi.UnexpectedException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Logger;
@@ -11,13 +14,38 @@ import java.util.logging.Logger;
 /**
  * Created by BB20101997 on 12.06.2015.
  */
+
+//TODO: WHAT IS THIS EXATCTLY DOING???
 public class InitLoggers {
 
 	//BasicChatLogges
 	public final String BCL = "bb.chat";
 
 	//the Folder to save the logs into
-	public  File logFolder = new File("Log").getAbsoluteFile();
+	private   File logFolder = new File("Log").getAbsoluteFile();
+
+	public void setLogFolder(File f) throws UnexpectedException, NotDirectoryException, AccessException {
+		if(isInitialized()){
+			throw new UnexpectedException("It´s not allowed to change the logFolder after InitLoggers initialisation!"+System.lineSeparator()+"Sorry!");
+		}
+		else{
+			if(f.isDirectory()) {
+				if(f.canWrite()){
+					logFolder = f;
+				}
+				else{
+					throw new AccessException("Need write access to logFolder Directory!"+System.lineSeparator()+"Or how do you expect us to write the log File?");
+				}
+			}
+			else{
+				throw new NotDirectoryException("Expected a Directory for logFolder!"+System.lineSeparator()+"Is the name not self Explaining?");
+			}
+		}
+	}
+
+	public boolean isInitialized(){
+		return inst!=null;
+	}
 
 	private static InitLoggers inst;
 
@@ -27,6 +55,8 @@ public class InitLoggers {
 		}
 		return inst;
 	}
+
+	//00:00 23/24 .10.2015 what am I doing here trying to fix a bug with renaming!
 
 	private InitLoggers(){
 		initLogger(BCL,"BasicChat","BasicChat");
