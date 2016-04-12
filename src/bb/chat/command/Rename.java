accd8a0b99f3c0b8a982eec4420e17b8bdc68c62
BB20101997 <blessmann-bennet@t-online.de>
2016-04-12 21:41:09 +0200

    added loging fixing rename bug

--- src/bb/chat/command/Rename.java
+++ src/bb/chat/command/Rename.java
@@ -1,9 +1,8 @@
 package bb.chat.command;
 
-import bb.chat.chat.ChatActor;
 import bb.chat.interfaces.IChat;
+import bb.chat.interfaces.IChatActor;
 import bb.chat.interfaces.ICommand;
-import bb.chat.network.packet.chatting.ChatPacket;
 import bb.chat.network.packet.command.RenamePacket;
 import bb.net.enums.Side;
 
@@ -14,11 +13,6 @@ import bb.net.enums.Side;
 public class Rename implements ICommand {
 
 	@Override
-	public String[] getAlias() {
-		return new String[0];
-	}
-
-	@Override
 	public String getName() {
 
 		return "rename";
@@ -27,18 +21,17 @@ public class Rename implements ICommand {
 	@Override
 	public void runCommand(String commandLine, IChat iChat) {
 		String[] dS = commandLine.split(" ");
-		if(dS.length <= 2 || "Client".equals(dS[2]) || "SERVER".equals(dS[2])) {
+		if(dS.length <= 2 || "Client".equals(dS[2]) || "SERVER".equals(dS[2]) || "ALL".equals(dS[2])) {
 			return;
 		}
 		if(iChat.getIConnectionManager().getSide() == Side.CLIENT) {
 			iChat.getIConnectionManager().sendPackage(new RenamePacket(dS[1], dS[2]), iChat.getIConnectionManager().ALL());
 		} else {
-			ChatActor ica = iChat.getActorByName(dS[1]);
+			IChatActor ica = iChat.getActorByName(dS[1]);
 			if(ica != null) {
 				ica.setActorName(dS[2]);
-				iChat.getBasicChatPanel().println("[" + iChat.getLocalActor().getActorName() + "] " + dS[1] + " is now known as " + dS[2]);
-				iChat.getIConnectionManager().sendPackage(new ChatPacket(dS[1] + " is now known as " + dS[2], iChat.getLocalActor().getActorName()), iChat.getIConnectionManager().ALL());
-				iChat.getIConnectionManager().sendPackage(new RenamePacket(dS[1], dS[2]), iChat.getActorByName(dS[2]).getIIOHandler());
+				iChat.getBasicChatPanel().println("[" + iChat.getLOCAL().getActorName() + "] " + dS[1] + " is now known as " + dS[2]);
+				iChat.getIConnectionManager().sendPackage(new RenamePacket(dS[1], dS[2]), iChat.getIConnectionManager().ALL());
 			}
 		}
 	}
@@ -48,10 +41,4 @@ public class Rename implements ICommand {
 		//noinspection StringConcatenationMissingWhitespace
 		return new String[]{ICommand.COMMAND_INIT_CHAR +"rename <new Name>", "Used to rename you in Chat!"};
 	}
-
-	@Override
-	public boolean isDebugModeOnly() {
-		return false;
-	}
-
 }
