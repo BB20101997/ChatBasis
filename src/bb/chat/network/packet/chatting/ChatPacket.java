1923ec87d6f77a7317f34eb013e2255d10efa212
BB20101997 <blessmann-bennet@t-online.de>
2016-04-12 21:29:15 +0200

    now overriding toString() to make Logging easier

--- src/bb/chat/network/packet/chatting/ChatPacket.java
+++ src/bb/chat/network/packet/chatting/ChatPacket.java
@@ -5,6 +5,7 @@ import bb.net.packets.DataIn;
 import bb.net.packets.DataOut;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 
 /**
  * Created by BB20101997 on 30.08.2014.
@@ -34,5 +35,10 @@ public class ChatPacket extends APacket {
 	public void readFromData(DataIn dataIn) throws IOException {
 		Message = dataIn.readUTF();
 		Sender = dataIn.readUTF();
+	}
+
+	@Override
+	public String toString() {
+		return MessageFormat.format("[ChatPacket]Sender='{'{1}'}',Message='{'{0}'}'", Message, Sender);
 	}
 }
