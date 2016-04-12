522886132c70a858a736cdbcae9854eb360a55ea
BB20101997 <blessmann-bennet@t-online.de>
2016-04-12 21:33:04 +0200

    added Logging

--- src/bb/chat/events/EventHandler.java
+++ src/bb/chat/events/EventHandler.java
@@ -1,19 +1,34 @@
 package bb.chat.events;
 
+import bb.util.file.log.BBLogHandler;
+import bb.util.file.log.Constants;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by BB20101997 on 30.01.2015.
  */
 //a dummy eventHandler
+@SuppressWarnings("unused")
 public class EventHandler {
+	private final List<Method> mList = new ArrayList<>();
 
-	List<Method> mList = new ArrayList<>();
+	@SuppressWarnings("ConstantNamingConvention")
+	private static final Logger log;
+
+	static {
+		log = Logger.getLogger(EventHandler.class.getName());
+		log.addHandler(new BBLogHandler(Constants.getLogFile("ChatBasis")));
+	}
+
+
 
 	public void subscribeEvent(Method method) {
+		log.finer("Subscribing to an Event");
 		if(method.getParameterTypes().length == 1 && method.getParameterTypes()[0].isAssignableFrom(Event.class)) {
 			if(!mList.contains(method)) {
 				mList.add(method);
@@ -24,15 +39,14 @@ public class EventHandler {
 	}
 
 	public void distributeEvent(Event event) {
-		for(Method m : mList) {
-			if(m.getParameterTypes()[0].isAssignableFrom(event.getClass())) {
-				try {
-					m.invoke(event);
-				} catch(IllegalAccessException | InvocationTargetException e1) {
-					e1.printStackTrace();
-				}
+		log.finer("Distributing an Event");
+		mList.stream().filter(me -> me.getParameterTypes()[0].isAssignableFrom(event.getClass())).forEach(me -> {
+			try {
+				me.invoke(event);
+			} catch(IllegalAccessException | InvocationTargetException e1) {
+				e1.printStackTrace();
 			}
-		}
+		});
 
 	}
 
