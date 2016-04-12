247566f860ae142e7662c1c24a59ff5702fa9df5
BB20101997 <blessmann-bennet@t-online.de>
2016-04-12 21:39:10 +0200

    moving on to changing it to just send the complete commandline

--- src/bb/chat/network/packet/command/PermissionPacket.java
+++ src/bb/chat/network/packet/command/PermissionPacket.java
@@ -10,26 +10,48 @@ import java.io.IOException;
  * Created by BB20101997 on 28.11.2014.
  */
 public class PermissionPacket extends APacket {
-//TODO just send complet command
-	public String cmd;
-	public String restCmd;
+	//TODO remove partial commands
+	public  String cmd;
+	public  String restCmd;
+	private String completeCommand;
 
 	public PermissionPacket() {}
 
 	public PermissionPacket(String commandType, String restOfCommand) {
 		cmd = commandType;
 		restCmd = restOfCommand;
+		completeCommand = cmd + " " + restCmd;
+	}
+
+	public PermissionPacket(String command){
+		completeCommand = command;
+		String[] sA = completeCommand.split(" ", 1);
+		if(sA.length > 0) {
+			cmd = sA[0];
+			if(sA.length > 1) {
+				restCmd = sA[1];
+			}
+		}
+	}
+
+	public String getCommand(){
+		return completeCommand;
 	}
 
 	@Override
 	public void writeToData(DataOut dataOut) throws IOException {
-		dataOut.writeUTF(cmd);
-		dataOut.writeUTF(restCmd);
+		dataOut.writeUTF(completeCommand);
 	}
 
 	@Override
 	public void readFromData(DataIn dataIn) throws IOException {
-		cmd = dataIn.readUTF();
-		restCmd = dataIn.readUTF();
+		completeCommand = dataIn.readUTF();
+		String[] sA = completeCommand.split(" ", 1);
+		if(sA.length > 0) {
+			cmd = sA[0];
+			if(sA.length >= 2) {
+				restCmd = sA[1];
+			}
+		}
 	}
 }
