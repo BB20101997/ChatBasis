859444f0902d626ddd810bef9dee7b6840b4b9ee
BB20101997 <blessmann-bennet@t-online.de>
2016-08-10 03:23:41 +0200

    now does only generate new ResourceBundle if Locale changed

--- src/main/java/bb/chat/enums/Bundles.java
+++ src/main/java/bb/chat/enums/Bundles.java
@@ -1,20 +1,30 @@
 package bb.chat.enums;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
  * Created by BB20101997 on 08. Aug. 2016.
  */
 public enum Bundles {
-	BUTTON_LABEL("bb.chat.lang.ButtonLabel"),COMMAND("bb.chat.lang.Command"),LOG_TEXT("bb.chat.lang.LogText"),MESSAGE("bb.chat.lang.Message");
 
-	public final String BUNDLE_NAME;
 
-	Bundles(String name){
+	BUTTON_LABEL("bb.chat.lang.ButtonLabel"), COMMAND("bb.chat.lang.Command"), LOG_TEXT("bb.chat.lang.LogText"), MESSAGE("bb.chat.lang.Message");
+
+	public final String         BUNDLE_NAME;
+	private      Locale         lastLocale;
+	private      ResourceBundle bundle;
+
+	Bundles(String name) {
 		BUNDLE_NAME = name;
 	}
 
-	public ResourceBundle getResource(){
-		return ResourceBundle.getBundle(BUNDLE_NAME);
+	public ResourceBundle getResource() {
+		if(Locale.getDefault().equals(lastLocale) && bundle != null) {
+			return bundle;
+		} else {
+			lastLocale = Locale.getDefault();
+			return bundle = ResourceBundle.getBundle(BUNDLE_NAME, lastLocale);
+		}
 	}
 }
