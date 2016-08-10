06a25b6af34f4308567527f71954d11ec68ac891
BB20101997 <blessmann-bennet@t-online.de>
2016-08-10 03:17:55 +0200

    moved implementation of deprecated function to interface as default implementation

--- src/main/java/bb/chat/command/BasicCommandRegistry.java
+++ src/main/java/bb/chat/command/BasicCommandRegistry.java
@@ -14,10 +14,12 @@ import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+import static bb.chat.base.Constants.LOG_NAME;
+
 /**
  * Created by @author BB20101997 on 30.01.2015.
  */
-@SuppressWarnings("HardcodedFileSeparator")
+@SuppressWarnings({"HardcodedFileSeparator", "unused"})
 public class BasicCommandRegistry implements ICommandRegistry {
 
 	private final List<ICommand> commandList = new ArrayList<>();
@@ -28,25 +30,16 @@ public class BasicCommandRegistry implements ICommandRegistry {
 	static {
 		log = Logger.getLogger(BasicCommandRegistry.class.getName());
 		//noinspection DuplicateStringLiteralInspection
-		log.addHandler(new BBLogHandler(Constants.getLogFile("ChatBasis")));
+		log.addHandler(new BBLogHandler(Constants.getLogFile(LOG_NAME)));
 	}
-	
-	@Override
-	public final void addCommand(java.lang.Class<? extends ICommand> command) {
 
-		try {
-			ICommand com = command.getConstructor().newInstance();
-			commandList.add(com);
-		} catch(Exception e) {
-			e.printStackTrace();
-			//noinspection StringConcatenationMissingWhitespace
-			log.warning("Error while adding Command."+System.lineSeparator()+"Command may not be available.");
-		}
+	public final void addCommand(ICommand command){
+		commandList.add(command);
 	}
 
 	@Override
 	public final ICommand getCommand(String text) {
-	log.finer("Receiving Command named "+text);
+		log.finer("Receiving Command named " + text);
 		//name is prioritised over alias
 		//check for name first
 		for(ICommand command : commandList) {
@@ -55,9 +48,9 @@ public class BasicCommandRegistry implements ICommandRegistry {
 			}
 		}
 		//check for alias afterwards
-		for(ICommand command:commandList){
-			for(String alias:command.getAlias()){
-				if(text.equals(alias)){
+		for(ICommand command : commandList) {
+			for(String alias : command.getAlias()) {
+				if(text.equals(alias)) {
 					return command;
 				}
 			}
@@ -67,23 +60,29 @@ public class BasicCommandRegistry implements ICommandRegistry {
 
 	@Override
 	public boolean runCommand(String commandLine, Side side, IChat ich) {
-		log.fine("Running "+commandLine+" on the "+side+".");
+		log.fine("Running " + commandLine + " on the " + side + ".");
 		String[] strA = commandLine.split(" ");
 		strA[0] = strA[0].replace(ICommand.COMMAND_INIT_STRING, "");
 		ICommand command = getCommand(strA[0]);
 
-		if(command != null) {
-			if(side == Side.SERVER) {
-				command.runCommand(commandLine, ich);
-			} else {
-				command.runCommand(commandLine, ich);
-			}
-			return true;
-		}
-		log.fine(strA[0]+" is not a valid command!");
-		ich.getBasicChatPanel().println(MessageFormat.format(Bundles.MESSAGE.getResource().getString("invalid.command"),ich.getLOCALActor().getActorName()));
-		return false;
 
+		if(command != null) {
+			if(command.isDebugModeOnly() && !ich.isDebugMode()) {
+				ich.getBasicChatPanel().println(Bundles.MESSAGE.getResource().getString("debug.only"));
+				return false;
+			} else {
+				if(side == Side.SERVER) {
+					command.runCommand(commandLine, ich);
+				} else {
+					command.runCommand(commandLine, ich);
+				}
+				return true;
+			}
+		} else {
+			log.fine(strA[0] + " is not a valid command!");
+			ich.getBasicChatPanel().println(MessageFormat.format(Bundles.MESSAGE.getResource().getString("invalid.command"), ich.getLOCALActor().getActorName()));
+			return false;
+		}
 	}
 
 	@Override
