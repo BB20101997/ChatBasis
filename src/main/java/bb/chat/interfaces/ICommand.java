2efa825bb99b043bc220ce4146e833c06d71eb28
BB20101997 <blessmann-bennet@t-online.de>
2016-08-10 03:29:27 +0200

    made the IDE happy by not creating 0-lenght arrays everywhere

--- src/main/java/bb/chat/interfaces/ICommand.java
+++ src/main/java/bb/chat/interfaces/ICommand.java
@@ -2,6 +2,8 @@ package bb.chat.interfaces;
 
 import bb.chat.enums.Bundles;
 
+import static bb.chat.base.Constants.EMPTY_STRING_ARRAY;
+
 /**
  * @author BB20101997
  */
@@ -11,10 +13,9 @@ public interface ICommand {
 	char COMMAND_INIT_CHAR = '/';
 	@SuppressWarnings("HardcodedFileSeparator")
 	String COMMAND_INIT_STRING = "/";
-	String[] NO_ALIAS = new String[0];
 
 	default String[] getAlias(){
-		return NO_ALIAS;
+		return EMPTY_STRING_ARRAY;
 	}
 
 	/**
